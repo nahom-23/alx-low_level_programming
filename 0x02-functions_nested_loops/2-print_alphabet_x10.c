5e09375e051364516c497fe3cb521afacc862c80
nahom-23 <noahbirhanu23@gmail.com>
2023-02-21 13:31:04 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: On success 1.
+ */
+void print_alphabet_x10(void)
+{
+	int n, j;
+
+	j  = 0;
+
+	while (j < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
