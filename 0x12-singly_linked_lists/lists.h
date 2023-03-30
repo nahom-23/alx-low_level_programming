8f396b14f9beebfbe22d9e546979e6e97cc8684a
nahom-23 <noahbirhanu23@gmail.com>
2023-03-30 14:27:50 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,24 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: All prototypes used in the singly linked directory
- * are contained here and is part of the necessities for succesfull
- * program compilation.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
